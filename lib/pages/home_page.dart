87c59fc54fe09fbece50f15133c490d219079b35
KarlMathuthu <karlmathuthu07@gmail.com>
2025-03-22 12:39:53 +0200

    modified

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,10 @@ import 'package:fhc_app_task/pages/tabs/explore_tab.dart';
 import 'package:fhc_app_task/pages/tabs/home_tab.dart';
 import 'package:fhc_app_task/pages/tabs/leaderboards_tab.dart';
 import 'package:fhc_app_task/pages/tabs/settings_page.dart';
+import 'package:fhc_app_task/theme/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,6 +17,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<CompetitionModel> competitions = [];
+  int currentPage = 0;
 
   late List<Widget> pages;
 
@@ -23,6 +27,21 @@ class _HomePageState extends State<HomePage> {
     "assets/icons/explore.svg",
     "assets/icons/settings.svg",
   ];
+
+  SvgPicture tabIconWidget(int index) {
+    return SvgPicture.asset(
+      tabsSvgIcons[index],
+      width: 22,
+      height: 22,
+
+      colorFilter: ColorFilter.mode(
+        index == currentPage
+            ? AppColors.primaryColor
+            : AppColors.inActiveTabColor,
+        BlendMode.srcIn,
+      ),
+    );
+  }
 
   @override
   void initState() {
@@ -38,19 +57,76 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.white,
         title: Text("FH Creatives"),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              "assets/icons/search.svg",
+              width: 22,
+              height: 22,
+              colorFilter: ColorFilter.mode(Colors.black87, BlendMode.srcIn),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              "assets/icons/bell.svg",
+              width: 22,
+              height: 22,
+
+              colorFilter: ColorFilter.mode(Colors.black87, BlendMode.srcIn),
+            ),
+          ),
         ],
       ),
 
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Leaderboard"),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Explore"),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Settings"),
-        ],
+      bottomNavigationBar: Theme(
+        data: ThemeData(splashColor: Colors.transparent),
+        child: Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 10),
+                color: Colors.grey.shade100,
+                blurRadius: 20,
+                spreadRadius: 10,
+              ),
+            ],
+          ),
+          child: SizedBox(
+            height: 100,
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              selectedFontSize: 14,
+              unselectedFontSize: 14,
+              selectedLabelStyle: GoogleFonts.poppins(),
+              unselectedLabelStyle: GoogleFonts.poppins(),
+              selectedItemColor: AppColors.primaryColor,
+              unselectedItemColor: AppColors.inActiveTabColor,
+              backgroundColor: Colors.white,
+              currentIndex: currentPage,
+              onTap: (newPage) {
+                setState(() {
+                  currentPage = newPage;
+                });
+              },
+              items: [
+                BottomNavigationBarItem(icon: tabIconWidget(0), label: "Home"),
+                BottomNavigationBarItem(
+                  icon: tabIconWidget(1),
+                  label: "Leading",
+                ),
+                BottomNavigationBarItem(
+                  icon: tabIconWidget(2),
+                  label: "Explore",
+                ),
+                BottomNavigationBarItem(
+                  icon: tabIconWidget(3),
+                  label: "Settings",
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primaryColor = Color.fromARGB(255, 75, 135, 237);
+  static const primaryColor = Color.fromARGB(255, 135, 69, 241);
   static const inActiveTabColor = Color.fromARGB(255, 127, 134, 146);
 }
